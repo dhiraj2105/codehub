f3147c80a1da304676eb3322a9f5312fac0f23c9
Dhiraj <dkskp2005@gmail.com>
2025-01-02 17:42:09 +0530

    optimised

--- leetcode-cpp/easy/two-sum.cpp
+++ leetcode-cpp/easy/two-sum.cpp
@@ -3,17 +3,35 @@ using namespace std;
 
 vector<int> twoSum(vector<int> nums, int target)
 {
+    // brute force method
+    // for (int i = 0; i < nums.size(); i++)
+    // {
+    //     for (int j = i + 1; j < nums.size(); j++)
+    //     {
+    //         if (nums[i] + nums[j] == target)
+    //         {
+    //             return {i, j};
+    //         }
+    //     }
+    // }
+    // return {0, 0};
+
+    // BETTER WAY - HASHMAP
+
+    unordered_map<int, int> num_dict; // empty map
+
     for (int i = 0; i < nums.size(); i++)
     {
-        for (int j = i + 1; j < nums.size(); j++)
+        int complement = target - nums[i];
+        // check if complement exsists in map
+        if (num_dict.find(complement) != num_dict.end())
         {
-            if (nums[i] + nums[j] == target)
-            {
-                return {i, j};
-            }
+            // return the indeces of current number and the complement
+            return {num_dict[complement], i};
         }
+        num_dict[nums[i]] = i; // store current number and its index
     }
-    return {0, 0};
+    return {};
 }
 
 int main()
