01b996c6759b7e42d039a277349fada4ab3af049
Dhiraj <dkskp2005@gmail.com>
2025-01-03 21:58:30 +0530

    optimised

--- leetcode-cpp/easy/contains_duplicates.cpp
+++ leetcode-cpp/easy/contains_duplicates.cpp
@@ -3,22 +3,57 @@ using namespace std;
 
 bool containsDuplicate(vector<int> nums)
 {
-    bool isDuplicate = false;
-    sort(nums.begin(), nums.end());
-    int l = 0, r = 1;
-    while (r < nums.size())
+    // O(n log n)
+    // bool isDuplicate = false;
+    // sort(nums.begin(), nums.end());
+    // int l = 0, r = 1;
+    // while (r < nums.size())
+    // {
+    //     if (nums[l] == nums[r])
+    //     {
+    //         isDuplicate = true;
+    //     }
+    //     else
+    //     {
+    //         l++;
+    //     }
+    //     r++;
+    // }
+    // return isDuplicate;
+
+    // O(n)
+    unordered_set<int> seen;
+    for (int i = 0; i < nums.size(); i++)
     {
-        if (nums[l] == nums[r])
+        if (seen.find(nums[i]) != seen.end())
         {
-            isDuplicate = true;
+            return true;
         }
         else
         {
-            l++;
+            seen.insert(nums[i]);
         }
-        r++;
     }
-    return isDuplicate;
+    return false;
+
+    // O(n)
+    // unordered_map<int, int> freq;
+    // for (int i = 0; i < nums.size(); i++)
+    // {
+    //     if (freq.find(nums[i]) != freq.end())
+    //     {
+    //         freq[nums[i]]++;
+    //     }
+    //     else
+    //     {
+    //         freq[nums[i]] = 1;
+    //     }
+    //     if (freq[nums[i]] > 1)
+    //     {
+    //         return true;
+    //     }
+    // }
+    // return false;
 }
 
 int main()
@@ -26,7 +61,7 @@ int main()
     /*
     Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
     */
-    vector<int> nums = {1, 2, 3, 1};
+    vector<int> nums = {1, 2, 3, 4};
     cout << boolalpha << containsDuplicate(nums) << endl;
     return 0;
 }
