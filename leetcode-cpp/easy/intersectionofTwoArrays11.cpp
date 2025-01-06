02f66ff0061225a5c47dcd2f463d3c959acecf8a
Dhiraj <dkskp2005@gmail.com>
2025-01-06 21:56:55 +0530

    optimised

--- leetcode-cpp/easy/intersectionofTwoArrays11.cpp
+++ leetcode-cpp/easy/intersectionofTwoArrays11.cpp
@@ -4,14 +4,30 @@ using namespace std;
 vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
 {
     // brute force
+    // vector<int> res;
+    // for (int i : nums1)
+    // {
+    //     auto j = find(nums2.begin(), nums2.end(), i);
+    //     if (j != nums2.end())
+    //     {
+    //         res.push_back(i);
+    //         nums2.erase(j);
+    //     }
+    // }
+
+    // hashmap
     vector<int> res;
+    unordered_map<int, int> m;
     for (int i : nums1)
     {
-        auto j = find(nums2.begin(), nums2.end(), i);
-        if (j != nums2.end())
+        m[i]++;
+    }
+    for (int i : nums2)
+    {
+        if (m[i] > 0)
         {
             res.push_back(i);
-            nums2.erase(j);
+            m[i]--;
         }
     }
 
@@ -23,8 +39,8 @@ int main()
     /*
     Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
     */
-    vector<int> nums1 = {1, 2, 2, 1};
-    vector<int> nums2 = {2, 2};
+    vector<int> nums1 = {4, 9, 5};
+    vector<int> nums2 = {9, 4, 9, 8, 4};
 
     vector<int> result = intersect(nums1, nums2);
     for (int i = 0; i < result.size(); i++)
