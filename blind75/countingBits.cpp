8f9a541fff896f96ce381078e3f92b6b57efeb5b
Dhiraj <dkskp2005@gmail.com>
2025-02-12 19:28:07 +0530

    divide and count

---
+++ blind75/countingBits.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> countingBits(int n)
+{
+    vector<int> ans(n + 1, 0);
+    for (int i = 0; i <= n; i++)
+    {
+        int num = i;
+        int count = 0;
+        while (num)
+        {
+            count += num % 2;
+            num /= 2;
+        }
+        ans[i] = count;
+    }
+    return ans;
+}
+
+int main()
+{
+    /*
+    Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+    */
+    int n = 5;
+    vector<int> ans = countingBits(n);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+
+    return 0;
+}
