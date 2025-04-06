55383aef724ae306143902c19be616ceb8ce4354
Dhiraj <dkskp2005@gmail.com>
2025-04-06 19:24:59 +0530

    recusrion + memoization

--- dp/fibonacci.cpp
+++ dp/fibonacci.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int fib(int num)
 {
+    // This is the normal recusive solution
     if (num <= 1)
     {
         return num;
@@ -11,13 +12,28 @@ int fib(int num)
     return fib(num - 1) + fib(num - 2);
 }
 
+int fibdp(int num, vector<int> &dp)
+{
+    // This is the solution with dp
+    // TOP - DOWN approach | recusrion + memoization
+    if (num <= 1)
+    {
+        return num;
+    }
+
+    dp[num] = fibdp(num - 1, dp) + fibdp(num - 2, dp);
+
+    return dp[num];
+}
+
 int main()
 {
-    int n;
-    cout << "Enter number : ";
-    cin >> n;
+    int n = 7;
+    // cout << "The fibonacci of " << n << " is : " << fib(n) << endl;
 
-    cout << "The fibonacci of " << n << " is : " << fib(n) << endl;
+    vector<int> dp(n + 1, 0);
+
+    cout << "The fibonacci of " << n << " is : " << fibdp(n, dp) << endl;
 
     return 0;
 }
