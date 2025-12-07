01da4f87e4ba2af9844a009c0123b816c2949d20
Dhiraj <dkskp2005@gmail.com>
2025-12-07 17:35:13 +0530

    linear search

---
+++ neetcode150/array-hashing/array/simpleProblems.java
@@ -0,0 +1,38 @@
+/**
+ * 1. Searching Algorithms : Linear and Binary
+ * 2. Sorting Algorithms : Bubble Sort
+ * 3. Simple problems : reverse array, find max element
+ */
+
+class SearchingAlgorithms {
+    public static int linearSearch(int nums[], int target) {
+        System.out.println("---> linear search");
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+}
+
+public class simpleProblems {
+    // print array
+    static void printArray(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 10, 20, 40, 20, 50 };
+        int target = 40;
+
+        // Searching Algorithms
+        int linearIDX = SearchingAlgorithms.linearSearch(nums, target);
+
+        System.out.println("The target : " + target + " founded by linear search at index : " + linearIDX);
+    }
+}
