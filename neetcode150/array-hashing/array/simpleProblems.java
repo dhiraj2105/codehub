fd88a94e75b50563b5b73efb4a823bfee3c346d5
Dhiraj <dkskp2005@gmail.com>
2025-12-07 18:14:27 +0530

    binary search

--- neetcode150/array-hashing/array/simpleProblems.java
+++ neetcode150/array-hashing/array/simpleProblems.java
@@ -16,10 +16,32 @@ class SearchingAlgorithms {
         return 0;
     }
 
+    public static int binarySearch(int nums[], int target) {
+        // the array must be SORTED
+        SortingAlgorithms.bubbleSort(nums);
+        // logic
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (target == nums[mid]) {
+                return mid;
+            }
+            if (target < nums[mid]) {
+                high = mid - 1;
+            }
+            if (target > nums[mid]) {
+                low = mid + 1;
+            }
+        }
+
+        return 0;
+    }
 }
 
 class SortingAlgorithms {
-    int[] bubbleSort(int nums[]) {
+    public static int[] bubbleSort(int nums[]) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = 0; j < nums.length - 1 - i; j++) {
                 if (nums[j] > nums[j + 1]) {
@@ -38,17 +60,24 @@ public class simpleProblems {
     // print array
     static void printArray(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i] + " ");
+            System.out.print(nums[i] + " ");
         }
+        System.out.println("\n");
     }
 
     public static void main(String[] args) {
-        int nums[] = { 10, 20, 40, 20, 50 };
+        int nums[] = { 10, 30, 40, 20, 50 };
         int target = 40;
 
         // Searching Algorithms
         int linearIDX = SearchingAlgorithms.linearSearch(nums, target);
+        int binaryIDX = SearchingAlgorithms.binarySearch(nums, target);
 
         System.out.println("The target : " + target + " founded by linear search at index : " + linearIDX);
+        System.out.println("The target : " + target + " founded by binary search at index : " + binaryIDX);
+
+        // Sorting Algorithm
+        System.out.print("Array Sorted using bubble sort : ");
+        printArray(SortingAlgorithms.bubbleSort(nums));
     }
 }
