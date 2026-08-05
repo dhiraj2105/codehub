4dfe2649bad2d91c85aa6bbdfd9672676a60f2cc
dhiraj2105 <dkskp2005@gmail.com>
2026-08-05 14:36:46 +0530

    fixup

--- striverA2Z/Arrays/Easy/Largest_element.cpp
+++ striverA2Z/Arrays/Easy/Largest_element.cpp
@@ -2,7 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int bruteForce(int nums[], int size) {
+int best(int nums[], int size) {
   // compare each element with current maximum element
   int maxElement = nums[0];
 
@@ -22,14 +22,11 @@ int bruteForce2(int nums[], int size) {
   return nums[size-1];
 }
 
-int best(int nums[], int size) { return 0; }
-
 int main() {
   // Find the largest element from the array
   int arr[] = {2, 5, 1, 3, 0};
   int size = sizeof(arr) / sizeof(arr[0]);
 
-  cout << "Largest Element from the array : " << bruteForce(arr, size) << endl;
   cout << "Largest Element from the array : " << bruteForce2(arr, size) << endl;
   cout << "Largest Element from the array : " << best(arr, size) << endl;
 }
